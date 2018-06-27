e65d7ea3ba23c0a1183a7b436dbad99b0cea5525
Mosima <mosimagodfrey@gmail.com>
2018-06-28 01:26:34 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route} from 'react-router-dom'
 import HomePage from './App/screens/HomePage';
+import NavBar from './App/screens/Navbar';
+import Navbar from './App/screens/Navbar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route  path="/" exact component={HomePage} />
+      <div>
+          <Navbar />
+          <Route  path="/" exact component={HomePage} />
+        </div>
       </BrowserRouter>
     );
   }
