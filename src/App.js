e7783c3eef0d1e21f6bfc55fc8ec5da73571f987
Mosima <mosimagodfrey@gmail.com>
2018-06-28 10:15:50 +0200

    profile design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route} from 'react-router-dom'
 import HomePage from './App/screens/HomePage';
-import NavBar from './App/screens/Navbar';
 import Navbar from './App/screens/Navbar';
+import Profile from './App/screens/Profile';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
+      <div className="App">
           <Navbar />
           <Route  path="/" exact component={HomePage} />
+          <Route path="/profile" component={Profile} />
         </div>
       </BrowserRouter>
     );
